a77287c73d39a0677278afd40fefbbb8bfea7b33
ryanhartadi999 <ryanhartadi999@gmail.com>
2023-04-08 08:55:34 +0700

    Meet2

---
+++ meet1/controll_statement/temp.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var edge float64 = 20
+
+	// cubeVolume variable ini scope hanya pada block if - else if - else
+	if cubeVolume := math.Pow(edge, 3); cubeVolume > 10000 {
+		// fmt.Println(name) | Valid
+		fmt.Println("Lerge volumne cube. Volume:", cubeVolume)
+
+	} else if cubeVolume >= 5000 {
+		// fmt.Println(name) | Valid
+		fmt.Println("Medium volume cube. Volume:", cubeVolume)
+
+	} else {
+		fmt.Println("Small volume cube. Volume:", cubeVolume)
+	}
+}
